f2b3586549e03f03878c0654b415d28974cc9c04
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2023-04-08 09:07:46 +0500

    3 tasks have been done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,30 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> swappedMap=new HashMap<>();
+        Map<String,Integer> valueCounts=new HashMap<>();
+        for (Map.Entry<Integer,String>entry:sourceMap.entrySet()){
+            String value= entry.getValue();
+            Integer key=entry.getKey();
+
+            if (valueCounts.containsKey(value)){
+                int existingKey=valueCounts.get(value);
+                if (existingKey<key){
+                    continue;
+                }else {
+                    swappedMap.remove(value);
+                }
+            }
+            swappedMap.put(value,key);
+            valueCounts.put(value,key);
+
+        }
+
+
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        String[] words = sentence.toLowerCase().split("\\W+");
+        for (String word : words) {
+            if (sentence.isEmpty()){
+                return wordCountMap;
+            }else if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+        return wordCountMap;
     }
 }
+
